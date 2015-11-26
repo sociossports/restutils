34995d556f48e5d3f3f65dd961c8bba22d6f38f5
Bart van der Schoor <bart@placbv.nl>
2015-11-26 17:22:39 +0100

    dropped naive date strformating with isodate package

--- restutils/utils.py
+++ restutils/utils.py
@@ -1,6 +1,6 @@
 import json
 from urllib.parse import urlparse
-
+from isodate import datetime_isoformat
 from django.conf import settings
 from django.core.urlresolvers import resolve
 from django.http import Http404
@@ -13,7 +13,10 @@ def iso_date(date):
     if date is not None:
         if is_naive(date):
             date = make_aware(date, get_current_timezone())
-        return date.strftime("%Y-%m-%dT%H:%M:%S%z")
+        # NOTE naive formatting generates bad non-iso strings
+        # return date.strftime("%Y-%m-%dT%H:%M:%S%z")
+        # replaced with real isodate
+        return datetime_isoformat(date)
 
 
 def extract_from_uri(uri, fields):
